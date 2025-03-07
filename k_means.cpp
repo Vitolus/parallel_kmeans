f8f9127f83951c7097d8663dcc14e09526691875
Vitolus <vito.davide00@gmail.com>
2025-03-07 16:19:26 +0100

    add constructor and virtual distance methods

--- k_means.cpp
+++ k_means.cpp
@@ -1,1 +1,5 @@
 #include "k_means.h"
+
+k_means::k_means(const int k, const int batchSize, const int maxIter) : k(k), batchSize(batchSize), maxIter(maxIter) {
+    centroids.resize(k);
+}
--- k_means.h
+++ k_means.h
@@ -1,12 +1,19 @@
 #ifndef K_MEANS_H
 #define K_MEANS_H
 
+#include <vector>
 
+class k_means{
+public:
+    k_means(int k, int batchSize, int maxIter);
+    virtual ~k_means() = default;
+    virtual float distance(const std::vector<float>& a, const std::vector<float>& b) = 0;
 
-class k_means {
-
+protected:
+    int k;
+    int batchSize;
+    int maxIter;
+    std::vector<std::pair<float, int>> centroids; // (ci, vi)
 };
-
-
 
 #endif //K_MEANS_H
