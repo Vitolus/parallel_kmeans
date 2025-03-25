85f0155c979e4441299f5328f6992fa35b74ea98
Vitolus <vito.davide00@gmail.com>
2025-03-25 22:56:32 +0100

    add parallelism to other methods

--- k_means.cpp
+++ k_means.cpp
@@ -58,6 +58,7 @@ std::pair<float, float> k_means::fit(const float tol){
 
 float k_means::euclideanDistance(const std::vector<float>& x, const int c_idx) const{
     float sum = 0.0;
+    // no improvement with parallelization
     for(auto i = 0; i < 784; i++){ // calculate the Euclidean distance between a data point and a centroid
         sum += std::pow(x[i] - centroids[c_idx][i], 2); // sum of squared differences
     }
@@ -65,18 +66,14 @@ float k_means::euclideanDistance(const std::vector<float>& x, const int c_idx) c
 }
 
 std::vector<std::vector<float>> k_means::sampleData() const{
-    std::vector<std::vector<float>> batch;
+    std::vector<std::vector<float>> batch(batchSize);
     std::vector<int> indices(dataset.size());
     std::iota(indices.begin(), indices.end(), 0); // fill the vector with 0, 1, 2, ..., dataset.size()-1
     std::mt19937 gen(666);
     std::shuffle(indices.begin(), indices.end(), gen);
-    // for(int i = indices.size()-1; i >= 0; i--){
-    //     std::uniform_int_distribution<int> dis(0, i);
-    //     std::swap(indices[i], indices[dis(gen)]); // shuffle the indices
-    // }
-    batch.reserve(batchSize);
-for(auto i = 0; i < batchSize; i++){ // sample batchSize shuffled data points
-        batch.push_back(dataset[indices[i]]);
+    #pragma omp parallel for if(n_threads > 1) num_threads(n_threads) schedule(dynamic)
+    for(auto i = 0; i < batchSize; i++){ // sample batchSize shuffled data points
+        batch[i] = dataset[indices[i]];
     }
     return batch;
 }
@@ -101,6 +98,7 @@ int k_means::findCentroidIdx(const std::vector<float>& x) const{
 void k_means::updateCentroid(const std::vector<float>& x, std::vector<int>& counts, const int idx){
     counts[idx] += 1; // add one to the count of data points assigned to the centroid
     const float lr = 1.0 / counts[idx]; // learning rate decays with the number of data points assigned to the centroid
+    // no improvement with parallelization
     for(auto i = 0; i < 784; i++){ // update the centroid based on a data point
         centroids[idx][i] = (1 - lr) * centroids[idx][i] + lr * x[i]; // lower lr, less weight to the new data point
     }
@@ -110,8 +108,13 @@ void k_means::scanAssign(const std::vector<std::vector<float>>& batch){
     for(auto& cluster : clusters){
         cluster.clear();
     }
+    #pragma omp parallel for if(n_threads > 1) num_threads(n_threads) schedule(dynamic)
     for(auto i = 0; i < batch.size(); i++){ // assign data points to the closest centroid
-        clusters[findCentroidIdx(batch[i])].push_back(i);
+        int idx = findCentroidIdx(batch[i]);
+        #pragma omp critical
+        {
+            clusters[idx].push_back(i);
+        }
     }
     for(auto& cluster : clusters){
         cluster.shrink_to_fit();
